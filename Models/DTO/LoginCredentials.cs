ef1d37518b4cd61a94e90f94064fb307b3327b5b
Ilya <thirdilno@gmail.com>
2023-11-21 23:45:05 +0700

    feat(models): add LoginCredentials class
    Add a LoginCredentials class that represents login data of a user.

---
+++ Models/DTO/LoginCredentials.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlogApi.Models.DTO;
+
+public class LoginCredentials
+{
+    [Required]
+    [EmailAddress]
+    public string Email { get; set; }
+    
+    [Required]
+    [MinLength(1)]
+    public string Password { get; set; }
+}
