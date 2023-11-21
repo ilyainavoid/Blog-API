0a54a096290bfd0b2b8d46cce1d72f8c947f976e
Ilya <thirdilno@gmail.com>
2023-11-21 21:11:14 +0700

    feat(models): add CommunityDto class
    Add CommunityDto class to represent a data about communities. This model doesn't include information about community administrators.

---
+++ Models/DTO/CommunityDto.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace BlogApi.Models.DTO;
+
+public class CommunityDto
+{
+    [Required]
+    public string Id { get; set; }
+    
+    [Required]
+    public DateTime CreateTime { get; set; }
+    
+    [Required]
+    [MinLength(1)]
+    public string Name { get; set; }
+    
+    public string? Description { get; set; }
+
+    [Required] 
+    public bool IsClosed { get; set; } = false;
+
+    [Required] 
+    public int SubscribersCount { get; set; } = 0;
+}
