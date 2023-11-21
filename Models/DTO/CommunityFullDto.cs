e2b2906626ee8136bf546bf54b36e00615e72be3
Ilya <thirdilno@gmail.com>
2023-11-21 22:22:20 +0700

    feat(models): add CommunityFullDto class
    Add CommunityFullDto class that represents full data about communities including administrators of community.
    List of UserDTO is used for storing all administrators of community.

---
+++ Models/DTO/CommunityFullDto.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlogApi.Models.DTO;
+
+public class CommunityFullDto
+{
+    [Required]
+    public Guid Id { get; set; }
+    
+    [Required]
+    public DateTime CreateTime { get; set; }
+    
+    [Required]
+    [MinLength(1)]
+    public string Name { get; set; }
+    
+    public string? Description { get; set; }
+
+    [Required] 
+    public bool IsClosed { get; set; } = false;
+
+    [Required] 
+    public int SubscribersCount { get; set; } = 0;
+    
+    [Required]
+    public List<UserDto> Administrators { get; set; }
+}
