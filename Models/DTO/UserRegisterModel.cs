d9920d7ad6a5dee2a1be60235ef30c2c2ec2e88f
Ilya <thirdilno@gmail.com>
2023-11-22 01:08:24 +0700

    feat(models): add UserRegisterMode class
    Add UserRegisterModel class that represent a structured data for signing up.

---
+++ Models/DTO/UserRegisterModel.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+using BlogApi.Models.Enums;
+
+namespace BlogApi.Models.DTO;
+
+public class UserRegisterModel
+{
+    [Required]
+    [MinLength(1)]
+    public string FullName { get; set; }
+    
+    [Required]
+    [MinLength(6)]
+    public string Password { get; set; }
+    
+    [Required]
+    [EmailAddress]
+    [MinLength(1)]
+    public string Email { get; set; }
+    
+    public DateTime? BirthDate { get; set; }
+    
+    public Gender Gender { get; set; }
+    
+    [Phone]
+    public string? PhoneNumber { get; set; }
+}
