605a246fba6f6a8e7d69c49bf714c53184c00b60
Ilya <thirdilno@gmail.com>
2023-11-25 01:06:28 +0700

    feat(entities): add User entity class
    Add User class that keeps the information about user. It will be used in migrations.

---
+++ Models/Entities/User.cs
@@ -0,0 +1,14 @@
+﻿using BlogApi.Models.Enums;
+
+namespace BlogApi.Models.Entities;
+
+public class User
+{
+    public Guid Id { get; set; }
+    public string Password { get; set; }
+    public string FullName { get; set; }
+    public DateTime BirthDate { get; set; }
+    public Gender Gender { get; set; }
+    public string PhoneNumber { get; set; }
+    public string Email { get; set; }
+}
