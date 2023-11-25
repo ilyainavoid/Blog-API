a94da55f25e9fa426fb14bf6247bfc36780f9c5b
Ilya <thirdilno@gmail.com>
2023-11-25 23:53:25 +0700

    fix(entities): add data annotations into User
    Add data annotations to make some fields required ([Required]), to determine min length ([MinLength(x)])

--- Models/Entities/User.cs
+++ Models/Entities/User.cs
@@ -1,16 +1,35 @@
-﻿using BlogApi.Models.Enums;
+﻿using System.ComponentModel.DataAnnotations;
+using BlogApi.Models.Enums;
 
 namespace BlogApi.Models.Entities;
 
-public class User
+public class User(List<Post> posts)
 {
+    [Required]
     public Guid Id { get; set; }
+    
+    [Required]
+    public DateTime CreateTime { get; set; }
+    
+    [Required]
     public string Password { get; set; }
+    
+    [Required]
+    [MinLength(1)]
     public string FullName { get; set; }
-    public DateTime BirthDate { get; set; }
+    
+    public DateTime? BirthDate { get; set; }
+    
+    [Required]
     public Gender Gender { get; set; }
-    public string PhoneNumber { get; set; }
+    
+    [Required]
+    [MinLength(1)]
     public string Email { get; set; }
     
+    public string? PhoneNumber { get; set; }
+    
     public List<Comment> Comments { get; set; }
+    public List<Like> Likes { get; set; }
+    public List<Post> Posts = posts;
 }
