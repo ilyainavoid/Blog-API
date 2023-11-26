025e830d4e0c7b77fc697fb4dfceee53d05fe5d8
Ilya <thirdilno@gmail.com>
2023-11-27 03:10:57 +0700

    feat(entities): add CommunityAdministrator and CommunitySubscriber
    Add CommunityAdministrator to create separate table for community admins. By the same logic, add CommunitySubscriber to create separate table for community subscribers.

---
+++ Models/Entities/CommunityAdministrator.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlogApi.Models.Entities;
+
+public class CommunityAdministrator
+{
+    [Required]
+    public Guid UserId { get; set; }
+    public User User { get; set; }
+
+    [Required]
+    public Guid CommunityId { get; set; }
+    public Community Community { get; set; }
+}
