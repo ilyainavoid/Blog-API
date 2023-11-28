4baac8075f48fe43e347c5db25d53af7a7e782ba
Ilya <thirdilno@gmail.com>
2023-11-28 23:30:47 +0700

    feat: add generic repository class and interface
    Add a generic implementation of the repository class and interface. Create BaseRepository<T>, providing operations with db such as insert, delete, update, and get and an interface, IBaseRepository<T>, has been added to define the contract for the repository.

---
+++ Repositories/BaseRepository.cs
@@ -0,0 +1,61 @@
+﻿using System.Linq.Expressions;
+using BlogApi.Repositories.Interfaces;
+using BlogApi.Services;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlogApi.Repositories;
+
+public class BaseRepository<T> : IBaseRepository<T> where T : class
+{
+    private readonly AppDbContext _context;
+
+    public BaseRepository(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    public T? Get(Guid id)
+    {
+        return _context.Set<T>().Find(id);
+    }
+
+    public List<T> GetAll()
+    {
+        return _context.Set<T>().ToList();
+    }
+
+    public IEnumerable<T> GetSpecified(Expression<Func<T, bool>> expression)
+    {
+        return _context.Set<T>().Where(expression);
+    }
+
+    public async Task Insert(T entity)
+    {
+        await _context.Set<T>().AddAsync(entity);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task Delete(T entity)
+    {
+        _context.Set<T>().Remove(entity);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task Update(T entity)
+    {
+        _context.Set<T>().Update(entity);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task InsertCollection(List<T> entities)
+    {
+        await _context.Set<T>().AddRangeAsync(entities);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task DeleteCollection(List<T> entities)
+    {
+        _context.Set<T>().RemoveRange(entities);
+        await _context.SaveChangesAsync();
+    }
+}
