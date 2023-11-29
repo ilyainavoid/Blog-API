500a44b0844c700e4e3e34b762b001ed9e380ebc
Ilya <thirdilno@gmail.com>
2023-11-29 18:55:59 +0700

    feat(user-service): create IUserService
    Create IUserService with Logic, Register, Logout, GetProfileInfo, EditProfileInfo methods to be implemented in UserService class.

---
+++ Services/IUserService.cs
@@ -0,0 +1,12 @@
+﻿using BlogApi.Models.DTO;
+
+namespace BlogApi.Services;
+
+public interface IUserService
+{
+    Task<TokenResponse> Register(UserRegisterModel userRegisterModel);
+    Task<TokenResponse> Login(LoginCredentials credentials);
+    Task<Response> Logout(string token);
+    Task<UserDto> GetProfileInfo(Guid id);
+    Task EditProfileInfo(Guid id, UserEditModel userEditModel);
+}
