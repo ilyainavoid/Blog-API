bb8fc0693c4814ac059016bafe34e0bde3d4e203
Ilya <thirdilno@gmail.com>
2023-12-01 03:03:02 +0700

    feat(tag-service): create service and implement endpoint logic
    Create TagService that implements ITagService. Implement GetAllTags() method using generic repository.

---
+++ Services/TagService.cs
@@ -0,0 +1,20 @@
+﻿using BlogApi.Models.Entities;
+using BlogApi.Repositories;
+using BlogApi.Repositories.Interfaces;
+
+namespace BlogApi.Services;
+
+public class TagService : ITagService
+{
+    private readonly IBaseRepository<Tag> _tagRepository;
+    
+    public TagService(IBaseRepository<Tag> tagRepository)
+    {
+        _tagRepository = tagRepository;
+    }
+    
+    public async Task<List<Tag>> GetAllTags()
+    {
+        return await _tagRepository.GetAll();
+    }
+}
