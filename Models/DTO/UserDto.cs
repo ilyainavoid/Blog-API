d6b87e918879c1f35ef3d2d98a171a95bc77ba83
Ilya <thirdilno@gmail.com>
2023-12-01 02:17:42 +0700

    feat(dto): add mobile phone validation
    Add data annotation that checks if field value matches the given regular expression in editModel, registerModel, userDto.

--- Models/DTO/UserDto.cs
+++ Models/DTO/UserDto.cs
@@ -25,5 +25,7 @@ public class UserDto
     [MinLength(1)]
     public string Email { get; set; }
     
+    [Phone]
+    [RegularExpression(@"^\+7 \(\d{3}\) \d{3}-\d{2}-\d{2}$")]
     public string? PhoneNumber { get; set; }
 }
