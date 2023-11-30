c51ee550344d5f17809b3fb29eea943ad730a0fa
Ilya <thirdilno@gmail.com>
2023-12-01 02:29:27 +0700

    feat(user-controller): add logout and get/put profile endpoints
    in UserController: change routing logic from taking endpoint name from action to specifying endpoint name using data annotation. Add and implement logout and get/put profile endpoints.

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -1,10 +1,15 @@
-﻿using BlogApi.Models.DTO;
+﻿using System.Net;
+using System.Security.Claims;
+using BlogApi.Models.DTO;
+using BlogApi.Models.Entities;
 using BlogApi.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BlogApi.Controllers;
 
-[Route("api/account/[action]")]
+[Route("api/account")]
 [ApiController]
 public class UserController : ControllerBase
 {
@@ -16,7 +21,7 @@ public class UserController : ControllerBase
         _userService = userService;
     }
 
-    [HttpPost]
+    [HttpPost("register")]
     public async Task<ActionResult<TokenResponse>> Register([FromBody] UserRegisterModel userRegisterModel)
     {
         if (!ModelState.IsValid)
@@ -28,7 +33,7 @@ public class UserController : ControllerBase
         return Ok(response);
     }
 
-    [HttpPost]
+    [HttpPost("login")]
     public async Task<ActionResult<TokenResponse>> Login([FromBody] LoginCredentials loginCredentials)
     {
         if (!ModelState.IsValid)
@@ -38,5 +43,61 @@ public class UserController : ControllerBase
 
         var response = await _userService.Login(loginCredentials);
         return Ok(response);
+    }
+    
+    [Authorize]
+    [HttpPost("logout")]
+    public async Task<ActionResult<Response>> Logout()
+    {
+        var authorizationHeader = Request.Headers["Authorization"].FirstOrDefault();
+
+        if (authorizationHeader == null)
+        {
+            return BadRequest("Authorization header is missing");
+        }
+
+        var token = authorizationHeader.Split(" ").Last();
+        try
+        {
+            await _userService.Logout(token);
+            return Ok();
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(500, "Internal Server Error");
+        }
+    }
+
+    [Authorize]
+    [HttpGet("profile")]
+    public async Task<ActionResult<UserDto>> GetProfile()
+    {
+
+        var idClaim = HttpContext.User.FindFirst("id");
+        UserDto response = new UserDto();
+        if (idClaim != null) {
+
+            if (Guid.TryParse(idClaim.Value, out var userId))
+            {
+                response = await _userService.GetProfileInfo(userId);
+            }
+        }
+        return Ok(response);
+    }
+    
+    [Authorize]
+    [HttpPut("profile")]
+    public async Task<ActionResult<UserDto>> EditProfile([FromBody] UserEditModel userEditModel)
+    {
+
+        var idClaim = HttpContext.User.FindFirst("id");
+        if (idClaim != null) {
+
+            if (Guid.TryParse(idClaim.Value, out var userId))
+            {
+                await _userService.EditProfileInfo(userId, userEditModel);
+            }
+        }
+        return Ok();
     }
 }
