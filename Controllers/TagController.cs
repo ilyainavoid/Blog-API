79134c81260e9b229d4813e3e27e7a25c95d4867
Ilya <thirdilno@gmail.com>
2023-12-01 20:33:28 +0700

    feat(tag-controller): create tag controller
    Implemented a new TagController to manage tag requests. The controller includes endpoints for showing all tags. When controller gets a request on appropriate route, it calls a tagService method.

---
+++ Controllers/TagController.cs
@@ -0,0 +1,31 @@
+﻿using BlogApi.Models.DTO;
+using BlogApi.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlogApi.Controllers;
+
+[Route("api/tag")]
+[ApiController]
+public class TagController : ControllerBase
+{
+    private readonly ITagService _tagService;
+
+    public TagController(ITagService tagService)
+    {
+        _tagService = tagService;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<List<TagDto>>> GetAllTags()
+    {
+        try
+        {
+            var response = await _tagService.GetAllTags();
+            return Ok(response);
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(500, "Error occured");
+        }
+    }
+}
