fe11a31555afaa1cf7e27974d909c6c5a3e79307
Ilya <thirdilno@gmail.com>
2023-12-01 20:57:05 +0700

    feat(auto-mapper): add mapper config for mapping tag classes
    Add a new feature in the auto-mapper module. Specifically, it adds a mapper configuration to determine the mapping of tag classes.

---
+++ Profiles/TagProfile.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using BlogApi.Models.DTO;
+using BlogApi.Models.Entities;
+
+namespace BlogApi.Profiles;
+
+public class TagProfile : Profile
+{
+    public TagProfile()
+    {
+        CreateMap<Tag, TagDto>();
+    }
+}
