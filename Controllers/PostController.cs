a6ab48bc6168e4ae689761248bb115493b95d2e4
Ilya <thirdilno@gmail.com>
2023-12-06 17:12:00 +0700

    feat: add post controller class
    Add PostController class that manages post-related queries. Add a method for every endpoint.

---
+++ Controllers/PostController.cs
@@ -0,0 +1,53 @@
+﻿using BlogApi.Models.DTO;
+using BlogApi.Services.Posts;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlogApi.Controllers;
+
+[ApiController]
+[Route("api/post")]
+public class PostController : ControllerBase
+{
+    private readonly IPostService _postService;
+
+    public PostController(IPostService postService)
+    {
+        _postService = postService;
+    }
+    
+    [Authorize]
+    [HttpGet]
+    public async Task<ActionResult<PostPagedListDto>> GetPosts([FromQuery] QueryParametersPost parameters)
+    {
+        throw new NotImplementedException();
+    }
+    
+    [Authorize]
+    [HttpPost]
+    public async Task<ActionResult<Guid>> CreatePost(CreatePostDto model)
+    {
+        throw new NotImplementedException();
+    }
+
+    [Authorize]
+    [HttpPost("{id}")]
+    public async Task<ActionResult<PostFullDto>> GetPostInfo(Guid id)
+    {
+        throw new NotImplementedException();
+    }
+
+    [Authorize]
+    [HttpPost("{postId}/like")]
+    public async Task<ActionResult<Response>> AddLike(Guid postId)
+    {
+        throw new NotImplementedException();
+    }
+    
+    [Authorize]
+    [HttpDelete("{postId}/like")]
+    public async Task<ActionResult<Response>> DeleteLike(Guid postId)
+    {
+        throw new NotImplementedException();
+    }
+}
