4d1d3ae40311b0ca6b250425cd16e4e38330ad32
Ilya <thirdilno@gmail.com>
2023-12-07 01:02:35 +0700

    fix(entities): edit Like entity class
    Add AuthorId and PostId in order to not get a whole Post or Author entity if only AuthorId or PostId is needed.
    Add migration and update database.

--- Models/Entities/Like.cs
+++ Models/Entities/Like.cs
@@ -3,8 +3,11 @@
 public class Like
 {
     public Guid Id { get; set; }
+    
+    public Guid AuthorId { get; set; }
     public User Author { get; set; }
-
+    
+    public Guid PostId { get; set; }
     public Post Post { get; set; }
 
 }
