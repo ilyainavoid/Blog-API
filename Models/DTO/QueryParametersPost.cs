e8673e7959437f35f7000e91e824dcc3a8b597c2
Ilya <thirdilno@gmail.com>
2023-12-06 16:36:48 +0700

    feat(query-params): create a class for post filters
    Add QueryParametersPost that represents info about post filtering parameters from query.

---
+++ Models/DTO/QueryParametersPost.cs
@@ -0,0 +1,18 @@
+﻿using BlogApi.Models.Enums;
+
+namespace BlogApi.Models.DTO;
+
+public class QueryParametersPost
+{
+    public class QueryParametersCommunity
+    {
+        public List<Guid>? Tags { get; set; }
+        public string? AuthorsName { get; set; }
+        public int MinReadingTime { get; set; }
+        public int MaxReadingTime { get; set; }
+        public PostSorting? Sorting { get; set; }
+        public bool OnlyMyCommunities { get; set; } = false;
+        public int CurrentPage { get; set; } = 1;
+        public int PageSize { get; set; } = 5;
+    }
+}
