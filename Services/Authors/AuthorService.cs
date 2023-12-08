124c31e6d023708bdedb2afb52210ed8fb56e5ab
Ilya <thirdilno@gmail.com>
2023-12-09 01:39:16 +0700

    feat(author-service): implement a method that return a list of authors
    Implement GetAuthors() that returns a list of AuthorDto. It represents authors (users who has created a post), namely postAmount, likesAmount and other information.

--- Services/Authors/AuthorService.cs
+++ Services/Authors/AuthorService.cs
@@ -1,5 +1,6 @@
 ﻿using BlogApi.Models.DTO;
 using BlogApi.Services.DbContexts;
+using Microsoft.EntityFrameworkCore;
 
 namespace BlogApi.Services.Authors;
 
@@ -12,8 +13,37 @@ public class AuthorService : IAuthorService
         _context = context;
     }
     
-    public Task<List<AuthorDto>> GetAuthors()
+    public async Task<List<AuthorDto>> GetAuthors()
     {
-        throw new NotImplementedException();
+        var result = new List<AuthorDto>();
+
+        //Take authors from users
+        var authors = await _context.Users
+            .Where(user => user.IsAuthor == true)
+            .Include(user => user.Posts)
+            .ToListAsync();
+
+        //Prepare result list
+        foreach (var user in authors)
+        {
+            //Calculate total amount of likes
+            int likesCount = _context.Likes.AsEnumerable().Count(like => user.Posts.Any(post => post.Id == like.PostId && post.AuthorId == user.Id));
+
+            //Map fields from user to author
+            var author = new AuthorDto
+            {
+                FullName = user.FullName,
+                BirthDate = user.BirthDate,
+                Gender = user.Gender,
+                Posts = user.Posts.Count(),
+                Likes = likesCount,
+                Created = user.CreateTime
+            };
+            
+            //Add author to the result list
+            result.Add(author);
+        }
+
+        return result;
     }
 }
